888d82159f6124edc101c1ae7d2a825a1c25ed90
penginryo <penginryo@gmail.com>
2016-01-07 23:53:16 -0800

    initial commit - tap count increment app

---
+++ xcode-projects/01_TapIncrement/01_TapIncrement/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  01_TapIncrement
+//
+//  Created by Ryo Makabe on 2016-01-07.
+//  Copyright © 2016 penginryo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	var count = 0
+	
+	@IBOutlet weak var countDisplay: UILabel!
+	
+	
+	
+	@IBAction func incrementButton(sender: UIButton) {
+		count += 1
+		countDisplay.text = "\(count)"
+	}
+	
+	@IBAction func resetButton(sender: UIButton) {
+		count = 0
+		countDisplay.text = "\(count)"
+	}
+	
+	
+	
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
