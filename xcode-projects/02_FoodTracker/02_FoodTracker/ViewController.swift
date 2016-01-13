b4fed79c8385fe7f1f7f740619b0c8bd5dd44c5e
penginryo <penginryo@gmail.com>
2016-01-13 00:55:21 -0800

    build a basic UI

---
+++ xcode-projects/02_FoodTracker/02_FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  02_FoodTracker
+//
+//  Created by Ryo Makabe on 2016-01-13.
+//  Copyright © 2016 penginryo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+}
+
