d2caf887a9be73bc1543ffd29079cee83c01531e
penginryo <penginryo@gmail.com>
2016-01-23 23:26:27 -0800

    add more sample data

--- xcode-projects/02_FoodTracker/02_FoodTracker/MealTableViewController.swift
+++ xcode-projects/02_FoodTracker/02_FoodTracker/MealTableViewController.swift
@@ -25,7 +25,13 @@ class MealTableViewController: UITableViewController {
 		let photo1 = UIImage(named: "meal1")!
 		let meal1 = Meal(name: "Soba", photo: photo1, rating: 5)!
 		
-		meals += [meal1]
+		let photo2 = UIImage(named: "meal2")!
+		let meal2 = Meal(name: "Caesar Salad", photo: photo2, rating: 4)!
+		
+		let photo3 = UIImage(named: "meal3")!
+		let meal3 = Meal(name: "Egg Benedict", photo: photo3, rating: 5)!
+		
+		meals += [meal1, meal2, meal3]
 	}
 
     override func didReceiveMemoryWarning() {
@@ -39,6 +45,7 @@ class MealTableViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		print(meals.count)
         return meals.count
     }
 
